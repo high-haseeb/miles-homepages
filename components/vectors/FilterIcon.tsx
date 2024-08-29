33b4c1336d730474a173982d143a55690c868b51
DrPrime01 <drprime.dev@gmail.com>
2024-08-29 15:30:21 +0100

    feat: updated tabs

--- components/vectors/FilterIcon.tsx
+++ components/vectors/FilterIcon.tsx
@@ -1,34 +1,35 @@
 export default function FilterIcon() {
-	return (
-		<svg
-			width="24"
-			height="24"
-			viewBox="0 0 24 24"
-			fill="none"
-			xmlns="http://www.w3.org/2000/svg"
-		>
-			<path
-				d="M14.32 19.0698C14.32 19.6798 13.92 20.4798 13.41 20.7898L12 21.6998C10.69 22.5098 8.87 21.5998 8.87 19.9798V14.6298C8.87 13.9198 8.47 13.0098 8.06 12.5098L4.21997 8.46976C3.70997 7.95976 3.31 7.05977 3.31 6.44977V4.12976C3.31 2.91976 4.22002 2.00977 5.33002 2.00977H18.67C19.78 2.00977 20.69 2.91975 20.69 4.02975V6.24976C20.69 7.05976 20.18 8.06976 19.68 8.56976"
-				stroke="#666867"
-				strokeWidth="1.5"
-				strokeMiterlimit="10"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-			/>
-			<path
-				d="M16.07 16.5191C17.8373 16.5191 19.27 15.0865 19.27 13.3192C19.27 11.5518 17.8373 10.1191 16.07 10.1191C14.3027 10.1191 12.87 11.5518 12.87 13.3192C12.87 15.0865 14.3027 16.5191 16.07 16.5191Z"
-				stroke="#666867"
-				strokeWidth="1.5"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-			/>
-			<path
-				d="M19.87 17.1191L18.87 16.1191"
-				stroke="#666867"
-				strokeWidth="1.5"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-			/>
-		</svg>
-	);
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-5 h-5 sm:w-6 sm:h-6"
+    >
+      <path
+        d="M14.32 19.0698C14.32 19.6798 13.92 20.4798 13.41 20.7898L12 21.6998C10.69 22.5098 8.87 21.5998 8.87 19.9798V14.6298C8.87 13.9198 8.47 13.0098 8.06 12.5098L4.21997 8.46976C3.70997 7.95976 3.31 7.05977 3.31 6.44977V4.12976C3.31 2.91976 4.22002 2.00977 5.33002 2.00977H18.67C19.78 2.00977 20.69 2.91975 20.69 4.02975V6.24976C20.69 7.05976 20.18 8.06976 19.68 8.56976"
+        stroke="#666867"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M16.07 16.5191C17.8373 16.5191 19.27 15.0865 19.27 13.3192C19.27 11.5518 17.8373 10.1191 16.07 10.1191C14.3027 10.1191 12.87 11.5518 12.87 13.3192C12.87 15.0865 14.3027 16.5191 16.07 16.5191Z"
+        stroke="#666867"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M19.87 17.1191L18.87 16.1191"
+        stroke="#666867"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
 }
--- components/vectors/SortIcon.tsx
+++ components/vectors/SortIcon.tsx
@@ -1,8 +1,31 @@
 export default function SortIcon() {
-    return <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M3 7H21" stroke="#666867" strokeWidth="1.5" strokeLinecap="round"/>
-    <path d="M6 12H18" stroke="#666867" strokeWidth="1.5" strokeLinecap="round"/>
-    <path d="M10 17H14" stroke="#666867" strokeWidth="1.5" strokeLinecap="round"/>
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-5 h-5 sm:w-6 sm:h-6"
+    >
+      <path
+        d="M3 7H21"
+        stroke="#666867"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
+      <path
+        d="M6 12H18"
+        stroke="#666867"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
+      <path
+        d="M10 17H14"
+        stroke="#666867"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
     </svg>
-    
+  );
 }
